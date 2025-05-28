eeab70d28f12136fb1d2ab66dececbbddc83693e
greetzkenny <greetzkenny@gmail.com>
2025-05-29 01:32:57 +0200

    removed chart

--- src/main/java/com/cryptoportfolio/CryptoPortfolioApplication.java
+++ src/main/java/com/cryptoportfolio/CryptoPortfolioApplication.java
@@ -2,10 +2,8 @@ package com.cryptoportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class CryptoPortfolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(CryptoPortfolioApplication.class, args);
